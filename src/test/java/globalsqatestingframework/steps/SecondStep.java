2e63af3d61c1d447e2a85f86b2f701983bbaa380
warrior <2020is111@stu.ucsc.cmb.ac.lk>
2025-11-12 21:00:48 +0530

    Started working on frames.

---
+++ src/test/java/globalsqatestingframework/steps/SecondStep.java
@@ -0,0 +1,11 @@
+package globalsqatestingframework.steps;
+
+import org.openqa.selenium.WebDriver;
+
+public class SecondStep {
+	private WebDriver driver_s;
+	
+	public SecondStep(WebDriver driver) {
+		this.driver_s = driver;
+	}
+}
